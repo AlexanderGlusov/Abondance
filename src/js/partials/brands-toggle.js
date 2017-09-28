afb16e522bfc46baf77483ba5296dc44cd72b10b
alexander <glusovaleksandr@gmail.com>
2017-09-28 13:06:28 +0300

    Fix desctop brands-slider

--- src/js/partials/brands-toggle.js
+++ src/js/partials/brands-toggle.js
@@ -3,8 +3,25 @@ $('.brands-toggle').click(function() {
   $(this).fadeOut('fast');
 });
 
+var slideNow = 1;
+var slideCount = $('#sliderWrapper').children().length - 2;
+
+function slideSwitch() {
+  if (slideNow === slideCount || slideNow <= 0 || slideNow > slideCount) {
+    $('#sliderWrapper').css({
+      'transform': 'translateX(0)'
+    });
+    slideNow = 1;
+  } else {
+      translateWidth = -320 * slideNow;
+      $('#sliderWrapper').css({
+        'transform': 'translateX(' + translateWidth + 'px)'
+      });
+      slideNow++;
+  }
+}
+
 $('.brands-toggle-desktop').click(function() {
-  $('.first-page').hide();
-  $('.second-page').fadeIn('fast');
+  slideSwitch();
 });
 
